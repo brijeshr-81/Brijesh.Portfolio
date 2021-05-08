816d822ef51378b24e8eb517e78b758a2cf7d72e
brijeshr-81 <brijeshr18@outlook.com>
2021-05-08 18:42:15 +0530

    React structure with the nav bar

---
+++ src/components/SideBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function SideBar() {
+    return (
+        <div>
+            <h1>My Portfolio</h1>
+        </div>
+    )
+}
+
+export default SideBar
